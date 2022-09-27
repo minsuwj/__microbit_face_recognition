3db5a73c7da94158fd147617cb97e0be39aeeafa
minsuwj <49870275+minsuwj@users.noreply.github.com>
2022-09-27 11:13:17 +0900

    과제용으로 내용 추가

--- main.ts
+++ main.ts
@@ -1,22 +1,25 @@
+let 초기화 = 0
 function Face_Recognition () {
+    초기화 = 0
     huskylens.initI2c()
     huskylens.initMode(protocolAlgorithm.ALGORITHM_FACE_RECOGNITION)
     while (초기화 == 0) {
         huskylens.request()
-        if (huskylens.isAppear(2, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
-            basic.showIcon(IconNames.Heart)
+        if (huskylens.isAppear(1, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+            basic.showIcon(IconNames.Sad)
             basic.pause(100)
             basic.clearScreen()
             초기화 = 1
         }
     }
-    music.playTone(262, music.beat(BeatFraction.Whole))
-    basic.showString("HI")
+    music.playTone(740, music.beat(BeatFraction.Whole))
+    music.playTone(622, music.beat(BeatFraction.Whole))
+    music.playTone(740, music.beat(BeatFraction.Whole))
+    music.playTone(622, music.beat(BeatFraction.Whole))
+    basic.showString("CRIMINAL")
     basic.pause(100)
-    초기화 = 0
+    초기화 += -1
 }
-let 초기화 = 0
-Face_Recognition()
 basic.forever(function () {
-	
+    Face_Recognition()
 })
